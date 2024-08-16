201570f57fe2d7f292c6ac785d9a28cdfb281885
X2SHADOW <hahandrew88@gmail.com>
2024-08-16 15:28:57 +0700

    Added method Hello

--- X2TEST/Assets/Scripts/Test.cs
+++ X2TEST/Assets/Scripts/Test.cs
@@ -7,12 +7,17 @@ public class Test : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("Hello world");
+        Hello();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void Hello()
+    {
+        print("Hello world");
     }
 }
